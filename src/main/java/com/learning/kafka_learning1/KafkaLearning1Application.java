d73ff789ff1ca8c2695387894df25606cfd243a1
praneeth reddy <praneethls4008@gmail.com>
2025-12-26 18:43:57 +0530

    init 1

---
+++ src/main/java/com/learning/kafka_learning1/KafkaLearning1Application.java
@@ -0,0 +1,13 @@
+package com.learning.kafka_learning1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaLearning1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaLearning1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/kafka_learning1/KafkaLearning1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.kafka_learning1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaLearning1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
